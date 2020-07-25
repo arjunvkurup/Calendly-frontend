bf7c755e5ebd6008c88b85026db2a5adb4a31784
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-07-25 10:21:53 +0530

    Added icons

---
+++ src/components/dashboard/Scheduler.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import events from '../events';
+import { Calendar, Views } from 'react-big-calendar';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.scss';
+import moment from 'moment';
+
+class
