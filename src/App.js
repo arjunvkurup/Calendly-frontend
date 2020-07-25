755b93e2fabc9a72052c4871b0e79852958e7c72
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-07-25 10:23:00 +0530

    created routes for pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route } from "react-router-dom";
-import Signin from './components/auth/Signin'
+import SignIn from './components/auth/SignIn'
+import SignUp from "./components/auth/SignUp";
+import Scheduler from "./components/dashboard/Scheduler";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
-          <Route exact path='/' component={Signin} />
+          <Route exact path='/' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
+            <Route path='/dashboard' component={Scheduler} />
         </Switch>
       </div>
     </BrowserRouter>
